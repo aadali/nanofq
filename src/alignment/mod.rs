fe29914333e77f5cf43a8c0ff0d039bb748a57db
aadali <aaaliyin@gmail.com>
2025-07-10 23:49:53 +0800

    Implement local alignment algorithm with scoring and operation tracking

--- src/alignment/mod.rs
+++ src/alignment/mod.rs
@@ -1,134 +1,308 @@
-/*
-use std::fmt::{Display, Formatter};
-use anyhow::Error;
-use crate::alignment::Direction::{Diag, Left, Up, Null};
+use crate::utils::{IS_MATCHED};
 
-pub mod pairwise_align;
-
-#[derive(PartialEq, Clone, Copy)]
-enum Direction {
-    Diag,
-    Left,
-    Up,
-    Null
+static REF: &[u8; 0] = b"";
+static READ: &[u8; 0] = b"";
+#[derive(Debug, Clone, PartialEq, Copy)]
+enum AlignmentOperation {
+    Match,
+    Subst,
+    Del,
+    Ins,
+    Null,
 }
 
-#[derive(Clone)]
-struct AlignConfig {
-    match_score: i32,
-    mismatch_score: i32,
-    gap_open_score: i32,
-    gap_extend_score: i32,
+enum SeqEnd {
+    End5,
+    End3,
 }
 
-struct LocalAlignMatrix {
-    query_len: usize,
-    target_len: usize,
-    score_matrix: Vec<Vec<i32>>,
-    direction_matrix: Vec<Vec<Direction>>,
-}
-
-impl LocalAlignMatrix {
-    fn new(query_len: usize, target_len: usize) -> Self {
-        // target seq as col, query seq as row
-        let score_row = vec![0i32; target_len + 1];
-        let direction_row = vec![Null; target_len + 1];
-        let mut score_matrix = vec![score_row; query_len + 1];
-        let mut direction_matrix = vec![direction_row; query_len + 1];
-        direction_matrix[0][0] = Diag;
-        for col in 1..target_len + 1 {
-            *(&mut direction_matrix[0][col]) = Left
-        }
-        for row in 1..query_len + 1 {
-            *(&mut direction_matrix[row][0]) = Up
-        }
-        LocalAlignMatrix {
-            query_len,
-            target_len,
-            score_matrix,
-            direction_matrix,
-        }
-    }
-}
-
-struct GlobalAlignMatrix {
-    
-}
-
-trait AlignMatrix {
-    fn get_score(&self, row: usize, col: usize) -> i32;
-    fn set_score(&mut self, row:usize, col:usize, score: i32) -> Result<(), anyhow::Error>;
-    fn get_direction(&self, row:usize, col: usize) -> Direction;
-    fn set_direction(&mut self, row: usize, col: usize, direction: Direction) -> Result<(),anyhow::Error>;
-}
-
-impl AlignMatrix  for LocalAlignMatrix{
-    fn get_score(&self, row: usize, col: usize) -> i32 {
-        self.score_matrix[row][col]
-    }
-
-    fn set_score(&mut self, row: usize, col: usize, score: i32) -> Result<(), Error> {
-        self.score_matrix[row][col] =score;
-        Ok(())
-    }
-
-    fn get_direction(&self, row: usize, col: usize) -> Direction {
-        self.direction_matrix[row][col]
-    }
-
-    fn set_direction(&mut self, row: usize, col: usize, direction: Direction) -> Result<(), Error> {
-        self.direction_matrix[row][col] = direction;
-        Ok(())
-    }
+#[derive(Debug, Clone)]
+struct Scores {
+    match_: i32,
+    mismatch: i32,
+    gap_open: i32,
+    gap_extend: i32,
 }
 
 #[derive(Debug)]
-struct AlignResult {
-    align_target: Vec<u8>,
-    align_line: Vec<u8>,
-    align_query: Vec<u8>,
+struct LocalAlignment<'a> {
+    // all index in this struct used 1-based coordinate system
+    reference: &'a [u8],
+    read: &'a [u8],
+    ref_range: (usize, usize),
+    read_range: (usize, usize),
     max_score: i32,
-    idx_of_max_score: (usize, usize), 
-    idx_of_start: (usize, usize), // (query_row_idx, target_col_idx)
+    read_map_ref_operations: Vec<AlignmentOperation>,
 }
 
-impl AlignResult {
-    fn new() -> Self {
-        AlignResult {
-            align_target: Vec::<u8>::new(),
-            align_line: Vec::<u8>::new(),
-            align_query: Vec::<u8>::new(),
-            max_score: 0i32,
-            idx_of_max_score: (0usize, 0usize),
-            idx_of_start: (0usize, 0usize),
+impl<'a> LocalAlignment<'a> {
+    fn pretty(&self, end: SeqEnd) -> String {
+        let mut align_ref_vec = Vec::<u8>::new();
+        let mut line_vec = Vec::<u8>::new();
+        let mut align_read_vec = Vec::<u8>::new();
+        let mut ref_idx = self.ref_range.0 - 1;
+        let mut read_idx = self.read_range.0 - 1;
+        for operation in &self.read_map_ref_operations {
+            match operation {
+                &AlignmentOperation::Match => {
+                    align_ref_vec.push(self.reference[ref_idx]);
+                    line_vec.push(b'|');
+                    align_read_vec.push(self.read[read_idx]);
+                    ref_idx += 1;
+                    read_idx += 1;
+                }
+                &AlignmentOperation::Subst => {
+                    align_ref_vec.push(self.reference[ref_idx]);
+                    line_vec.push(b'\\');
+                    align_read_vec.push(self.read[read_idx]);
+                    ref_idx += 1;
+                    read_idx += 1;
+                }
+                &AlignmentOperation::Del => {
+                    align_read_vec.push(b'-');
+                    line_vec.push(b'x');
+                    align_ref_vec.push(self.reference[ref_idx]);
+                    ref_idx += 1;
+                }
+                &AlignmentOperation::Ins => {
+                    align_read_vec.push(self.read[read_idx]);
+                    line_vec.push(b'x');
+                    align_ref_vec.push(b'-');
+                    read_idx += 1;
+                }
+                _ => {
+                    panic!("you could not in here")
+                }
+            }
+        }
+        debug_assert_eq!(ref_idx, self.ref_range.1);
+        debug_assert_eq!(read_idx, self.read_range.1);
+        let mut read_range_start = self.read_range.0 as isize;
+        let mut read_range_end = self.read_range.1 as isize;
+        let align_read_str = match end {
+            SeqEnd::End5 => {
+                format!(
+                    "{:>5} {} {:<5}",
+                    self.read_range.0,
+                    unsafe { std::str::from_utf8_unchecked(&align_read_vec) },
+                    self.read_range.1
+                )
+            }
+            SeqEnd::End3 => {
+                read_range_start = 0 - (self.read.len() as isize - read_range_start + 1);
+                read_range_end = 0 - (self.read.len() as isize - read_range_end + 1);
+                format!(
+                    "{:>5} {} {:<5}",
+                    read_range_start,
+                    unsafe { std::str::from_utf8_unchecked(&align_read_vec) },
+                    read_range_end
+                )
+            }
+        };
+        let line = format!("      {}      ", unsafe {
+            std::str::from_utf8_unchecked(&line_vec)
+        });
+        let align_ref_string = format!(
+            "{:>5} {} {:<5}",
+            self.ref_range.0,
+            unsafe { std::str::from_utf8_unchecked(&align_ref_vec) },
+            self.ref_range.1
+        );
+        format!(
+            "ReadAlignRange: [{}, {}]\nRefAlignRange: [{}, {}]\n{}\n{}\n{}",
+            read_range_start,
+            read_range_end,
+            self.ref_range.0,
+            self.ref_range.1,
+            align_read_str,
+            line,
+            align_ref_string
+        )
+    }
+}
+
+impl<'a> Default for LocalAlignment<'a> {
+    fn default() -> Self {
+        LocalAlignment {
+            reference: &REF[..],
+            read: &READ[..],
+            ref_range: (0, 0),
+            read_range: (0, 0),
+            max_score: i32::MIN,
+            read_map_ref_operations: vec![],
         }
     }
-    fn clear(&mut self) {
-        self.align_target.clear();
-        self.align_query.clear();
-        self.align_line.clear();
-        self.max_score = 0;
-        self.idx_of_start = (0, 0);
-        self.idx_of_max_score = (0, 0);
-    }
-    
-    fn reverse(&mut self) {
-        self.align_target.reverse();
-        self.align_line.reverse();
-        self.align_query.reverse();
-    }
 }
 
-impl Display for AlignResult{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let x = std::str::from_utf8(&self.align_target).unwrap();
-        unsafe {
-            write!(f, "target: {}\n        {}\nquery:  {}\n", std::str::from_utf8_unchecked(&self.align_target),
-                   std::str::from_utf8_unchecked(&self.align_line),
-                   std::str::from_utf8_unchecked(&self.align_query)
-            )
+#[derive(Debug, Clone)]
+struct LocalAligner {
+    row: usize,
+    col: usize,
+    scores: Scores,
+    score_matrix: Vec<Vec<i32>>,
+    align_matrix: Vec<Vec<AlignmentOperation>>,
+}
+
+impl LocalAligner {
+    fn new(row: usize, col: usize, scores: Scores) -> Self {
+        debug_assert!(scores.gap_extend < 0, "gap_extend must be negative int");
+        debug_assert!(scores.gap_open < 0, "gap_open must be negative int");
+        debug_assert!(scores.match_ > 0, "match must be positive int");
+        debug_assert!(scores.mismatch < 0, "mismatch must be positive int");
+        let score_matrix_row = vec![0i32; col + 1];
+        let mut score_matrix = vec![score_matrix_row; row + 1];
+        let mut align_matrix = vec![vec![AlignmentOperation::Null; col + 1]; row + 1];
+        for i in 0..col + 1 {
+            align_matrix[0][i] = AlignmentOperation::Ins;
+        }
+        for j in 0..row + 1 {
+            align_matrix[j][0] = AlignmentOperation::Del;
+        }
+        LocalAligner {
+            row,
+            col,
+            scores,
+            score_matrix,
+            align_matrix,
         }
     }
+
+    fn align<'a>(&mut self, reference: &'a [u8], read: &'a [u8]) -> LocalAlignment<'a> {
+        debug_assert!(self.row >= reference.len());
+        debug_assert!(self.col >= read.len());
+        let mut local_alignment = LocalAlignment::default();
+        local_alignment.reference = reference;
+        local_alignment.read = read;
+        for ref_row in 1..(reference.len() + 1) {
+            for read_col in 1..(read.len() + 1) {
+                let prev_score_from_diag = self.score_matrix[ref_row - 1][read_col - 1];
+                let prev_score_from_up = self.score_matrix[ref_row - 1][read_col];
+                let prev_score_from_left = self.score_matrix[ref_row][read_col - 1];
+                let (this_score_from_diag, diag_bases_operation) =
+                    if IS_MATCHED(&reference[ref_row - 1], &read[read_col - 1]) {
+                        (
+                            self.scores.match_ + prev_score_from_diag,
+                            AlignmentOperation::Match,
+                        )
+                    } else {
+                        (
+                            self.scores.mismatch + prev_score_from_diag,
+                            AlignmentOperation::Subst,
+                        )
+                    };
+                let this_score_from_up =
+                    if self.align_matrix[ref_row - 1][read_col] == AlignmentOperation::Del {
+                        prev_score_from_up + self.scores.gap_extend
+                    } else {
+                        prev_score_from_up + self.scores.gap_open
+                    };
+                let this_score_from_left =
+                    if self.align_matrix[ref_row][read_col - 1] == AlignmentOperation::Ins {
+                        prev_score_from_left + self.scores.gap_extend
+                    } else {
+                        prev_score_from_left + self.scores.gap_open
+                    };
+                let this_cell_score = *[
+                    this_score_from_diag,
+                    this_score_from_left,
+                    this_score_from_up,
+                    0,
+                ]
+                .iter()
+                .max()
+                .unwrap();
+                if this_cell_score > local_alignment.max_score {
+                    local_alignment.max_score = this_cell_score;
+                    local_alignment.ref_range.1 = ref_row;
+                    local_alignment.read_range.1 = read_col;
+                }
+                self.score_matrix[ref_row][read_col] = this_cell_score;
+                if this_cell_score == this_score_from_diag {
+                    self.align_matrix[ref_row][read_col] = diag_bases_operation;
+                } else if this_cell_score == this_score_from_left {
+                    self.align_matrix[ref_row][read_col] = AlignmentOperation::Ins;
+                } else {
+                    self.align_matrix[ref_row][read_col] = AlignmentOperation::Del
+                }
+            }
+        }
+
+        let mut trace_ref_row = local_alignment.ref_range.1;
+        let mut trace_read_col = local_alignment.read_range.1;
+        println!(
+            "{}-{}",
+            local_alignment.ref_range.1, local_alignment.read_range.1
+        );
+        loop {
+            let this_operation = self.align_matrix[trace_ref_row][trace_read_col];
+            local_alignment.read_map_ref_operations.push(this_operation);
+            if this_operation == AlignmentOperation::Match
+                || this_operation == AlignmentOperation::Subst
+            {
+                trace_ref_row -= 1;
+                trace_read_col -= 1;
+                if !(trace_read_col > 0
+                    && trace_read_col > 0
+                    && self.score_matrix[trace_ref_row][trace_read_col] > 0)
+                {
+                    local_alignment.ref_range.0 = trace_ref_row + 1;
+                    local_alignment.read_range.0 = trace_read_col + 1;
+                    break;
+                }
+            } else if this_operation == AlignmentOperation::Del {
+                trace_ref_row -= 1;
+                if !(trace_read_col > 0
+                    && trace_read_col > 0
+                    && self.score_matrix[trace_ref_row][trace_read_col] > 0)
+                {
+                    local_alignment.ref_range.0 = trace_ref_row + 1;
+                    local_alignment.read_range.0 = trace_read_col;
+                    break;
+                }
+            } else {
+                trace_read_col -= 1;
+                if !(trace_read_col > 0
+                    && trace_read_col > 0
+                    && self.score_matrix[trace_ref_row][trace_read_col] > 0)
+                {
+                    local_alignment.ref_range.0 = trace_ref_row;
+                    local_alignment.read_range.0 = trace_read_col + 1;
+                    break;
+                }
+            }
+        }
+        local_alignment.read_map_ref_operations.reverse();
+        local_alignment
+    }
 }
 
- */
+#[test]
+fn test_max() {
+    let v = [1, 23, 32];
+    let a = 20;
+    let b = 234;
+    let c = 90;
+    let x = *[a, b, c].iter().max().unwrap();
+    println!("{x}");
+    let x = *v.iter().max().unwrap();
+    println!("{x}");
+}
+
+pub fn test_alignment() {
+    let scores = Scores {
+        match_: 3,
+        mismatch: -3,
+        gap_open: -5,
+        gap_extend: -1,
+    };
+    let mut aligner = LocalAligner::new(80, 200, scores);
+    let reference = b"AAGGTTAACACAAAGACACCGACAACTTTCTTCAGCACCT";
+    // let read = b"TTTGTTAGTCTACTTCGTTCAGTTACGTATTGCTAAAGGTTAACACAAAGACACCGACAACTTTCTTCAGCACCTGCTTACGGTTCACTACTCACGACGAT";
+    let read = b"GTTAACCTACTTCGTTCAGTTACGTATTGCTAAGGTTAACACAAAGACACCGACAACTTCTTCAGCACCTGCTTACGGTTCACTACTCACGAC";
+    // let read = b"ATGGA";
+    // let reference = b"ATGCGA";
+    let alignment = aligner.align(reference, read);
+    println!("{:?}", alignment);
+    let x = alignment.pretty(SeqEnd::End5);
+    println!("{x}");
+}
---
+++ src/utils.rs
@@ -0,0 +1,62 @@
+use hashbrown::{HashMap, HashSet};
+use std::hint::assert_unchecked;
+use std::io::IoSliceMut;
+use std::sync::OnceLock;
+
+static DEGE_BASES: OnceLock<HashMap<u8, HashSet<u8>>> = OnceLock::new();
+static BASES: OnceLock<HashMap<u8, u8>> = OnceLock::new();
+
+pub fn get_dege_bases() -> &'static HashMap<u8, HashSet<u8>> {
+    DEGE_BASES.get_or_init(|| {
+        HashMap::from([
+            (b'R', HashSet::from([b'A', b'G'])),
+            (b'Y', HashSet::from([b'C', b'T'])),
+            (b'M', HashSet::from([b'C', b'A'])),
+            (b'K', HashSet::from([b'G', b'T'])),
+            (b'S', HashSet::from([b'C', b'G'])),
+            (b'W', HashSet::from([b'A', b'T'])),
+            (b'H', HashSet::from([b'A', b'T', b'C'])),
+            (b'B', HashSet::from([b'G', b'T', b'C'])),
+            (b'V', HashSet::from([b'G', b'A', b'C'])),
+            (b'D', HashSet::from([b'G', b'A', b'T'])),
+            (b'N', HashSet::from([b'G', b'A', b'T', b'C'])),
+        ])
+    })
+}
+
+pub fn get_bases() -> &'static HashMap<u8, u8> {
+    BASES.get_or_init(|| {
+        HashMap::from([
+            (b'A', b'T'),
+            (b'T', b'A'),
+            (b'G', b'C'),
+            (b'C', b'G'),
+            (b'a', b'T'),
+            (b't', b'A'),
+            (b'g', b'C'),
+            (b'c', b'G'),
+            (b'N', b'N'),
+            (b'n', b'N'),
+        ])
+    })
+}
+
+// ref_base from primer or reference can be dege base
+pub static IS_MATCHED: fn(&u8, &u8) -> bool = |ref_base, read_base| {
+    ref_base == read_base
+        || get_dege_bases()
+            .get(ref_base)
+            .map_or(false, |x| x.contains(read_base))
+};
+
+#[test]
+fn test_dege_base() {
+    assert!(IS_MATCHED(&b'V', &b'A'));
+    assert!(IS_MATCHED(&b'A', &b'A'));
+    assert!(!IS_MATCHED(&b'C', &b'A'));
+    // assert!(IS_MATCHED(&b'G', &b'V'));
+    assert!(IS_MATCHED(&b'B', &b'C'));
+    assert!(IS_MATCHED(&b'B', &b'T'));
+    assert!(IS_MATCHED(&b'B', &b'G'));
+    assert!(IS_MATCHED(&b'W', &b'T'));
+}
